87429142be20e1abb75594a5b6730f010150ce73
MuhammadShaheer1 <59112438+MuhammadShaheer1@users.noreply.github.com>
2023-12-27 10:14:27 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs not browser
+
+console.log(3+3) //code readiblity should be high
+console.log("Shaheer")
+
+let name = "Shaheer"
+let age = 22
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+
